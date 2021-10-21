fed7bb41c4b17e4f41ba57715a78c86782057f3f
George Lemmon <george.lemmon@gmail.com>
2021-10-21 11:45:03 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,16 @@
+var famousPerson = 'Conan the Barbarian';
+
+var isActive = false;
+
+famousPerson = 'Conan the Talk Show Host';
+
+var num3 = 3;
+var num9 = 8;
+var sum = num3 + num9;
+
+var animals = ['lions', 'tigers', 'bears'];
+animals.shift();
+
+let str = famousPerson;
+str.length;
+console.log('The number of characters in famousPerson is: ' + str.length);
