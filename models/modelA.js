c443812de1f46b98ce0d9306e72ecc5411bc2824
George Lemmon <george.lemmon@gmail.com>
2021-10-21 12:03:17 -0600

    Add object airport to modelA.js

---
+++ models/modelA.js
@@ -0,0 +1,21 @@
+//Airport
+
+var name = 'GALemmon International Airport';
+var airline = 'SuperLux Airlines';
+var numberOfPassengers = 75;
+var numberOfComfortAnimals = 4.5;
+var supportDucksAllowed = false;
+var masksRequired = true;
+var planeTypes = ['737', '777', 'A380'];
+var flightTimes = [0800, 1000, 1530, 2000];
+
+var airport = {
+  name: 'GALemmon International Airport',
+  airline: "SuperLux Airlines",
+  numberOfPassengers: 75,
+  numberOfComfortAnimals: 4.5,
+  supportDucksAllowed: false,
+  masksRequired: true,
+  planeTypes: ['737', '777', 'A380'],
+  flightTimes: [0800, 1000, 1530, 2000]
+}
