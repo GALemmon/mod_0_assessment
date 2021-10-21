435e358d9b14339ab4b1c4c3209fe8c6d1dc1226
George Lemmon <george.lemmon@gmail.com>
2021-10-21 12:15:52 -0600

    Add object motorcycle to modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+//Vehicle
+
+const motorcycle = {
+  wheels: 2,
+  horsepower: 120,
+  make: 'Triumph',
+  model: 'Thunderbird',
+  isBlue: true,
+  isCool: true,
+  gears: [1, 2, 3, 4, 5, 6],
+  needsMaintanence: ['oil change', 'valve check']
+};
